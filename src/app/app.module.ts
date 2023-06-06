48441359a271fa39cdeb5e936442e96e4441d182
anadiradze <anadi16@freeuni.edu.ge>
2023-06-06 16:37:32 +0400

    feat: create basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewTasksComponent } from './components/new-tasks/new-tasks.component';
+import { InProgressTasksComponent } from './components/in-progress-tasks/in-progress-tasks.component';
+import { FinishedTasksComponent } from './components/finished-tasks/finished-tasks.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ListContainerComponent } from './components/shared/list-container/list-container.component';
+import { ListItemComponent } from './components/shared/list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTasksComponent,
+    InProgressTasksComponent,
+    FinishedTasksComponent,
+    DashboardComponent,
+    ListContainerComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/in-progress-tasks/in-progress-tasks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-in-progress-tasks',
+  templateUrl: './in-progress-tasks.component.html',
+  styleUrls: ['./in-progress-tasks.component.css']
+})
+export class InProgressTasksComponent {
+
+}
