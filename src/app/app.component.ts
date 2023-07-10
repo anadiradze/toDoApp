41b3206ab7c618271e908a80e63158e2c92459d4
anadiradze <anadi16@freeuni.edu.ge>
2023-07-10 14:48:14 +0400

    feat: implement logger service for production mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { LoggerServiceService } from './shared/services/logger-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'toDoApp';
+  constructor(private loggerService: LoggerServiceService) {
+    this.loggerService.disableLogsinProd();
+  }
 }
---
+++ src/app/shared/services/logger-service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/app/environments/environment.prod';
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggerServiceService {
+  constructor() {}
+  disableLogsinProd() {
+    if (environment.production) {
+      console.log = function (): void {};
+    }
+  }
+}
